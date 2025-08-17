f76b8074a1c09c649ebe7110bd0763cca2fe1ce4
ehsan abbasifar <myehsanabbasifar@gmail.com>
2025-08-17 06:23:17 +0330

    feat : complete page logic for menu page

---
+++ .history/src/features/menu/PizzaItem_20250816040253.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+
+export default function PizzaItem({ data }) {
+  const dispatch = useDispatch()
+  return (
+    <li className="mx-auto font-Vazirmatn">
+      <div className="flex p-2 gap-x-3">
+        <div className="flex flex-col gap-y-1">
+          <h3 className="font-bold text-right">{data.pizza_name}</h3>
+          <p className="font-thin text-right">{data.description}</p>
+          <div className="flex justify-between w-full">
+            <button onClick={dis} className="bg-yellow-400 rounded-full p-1 px-3 ">
+              ثبت سفارش
+            </button>
+            <span>{data.price}$</span>
+          </div>
+        </div>
+        <div>
+          <img src={data.image} className="h-24" />
+        </div>
+      </div>
+    </li>
+  );
+}
---
+++ .history/src/features/menu/PizzaItem_20250817052410.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addItem } from "../../redux/orderItemsSlice";
+
+export default function PizzaItem({ data }) {
+  const [isSelected,setIsSelected]   = useState(false)
+  const dispatch = useDispatch();
+  const store = useSelector((store) => store.orderItems);
+  function handleClickOrdered () {
+dispatch(addItem(data))
+  }
+  return (
+    <li className="mx-auto font-Vazirmatn">
+      <div className="flex p-2 gap-x-3">
+        <div className="flex flex-col gap-y-1">
+          <h3 className="font-bold text-right">{data.pizza_name}</h3>
+          <p className="font-thin text-right">{data.description}</p>
+          <div className="flex justify-between w-full">
+            <button
+              type="click"
+              onClick={() => }
+              className="bg-yellow-400 cursor-pointer rounded-full p-1 px-3 "
+            >
+              ثبت سفارش
+            </button>
+            <span>{data.price}$</span>
+          </div>
+        </div>
+        <div>
+          <img src={data.image} className="h-24" />
+        </div>
+      </div>
+    </li>
+  );
+}
